678f204b94a1bef066055c494e01060c94e1d68a
David Mulholland <david@futr.ai>
2023-11-24 12:12:13 +0000

    REMOVE NODE MODULES

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // I have chosen to use ES6 modules in this project rather than common js require. To do this I have added   "type": "module" in the package json file. also to note es6 modules
 
 // importing environmental variables
-import './loadEnv.js';
+import "./loadEnv.js";
 
 // import express
 
-import express from 'express';
+import express from "express";
 const app = express();
 
 //cors variable
-import cors from 'cors';
+import cors from "cors";
 
 // morgan is a middle ware which will details all http requests in the console
 
@@ -22,16 +22,16 @@ let uri = process.env.mongoURI;
 app.use(cors());
 
 // morgan logs any http requests in the console for tracking.
-import morgan from 'morgan';
-app.use(morgan('combined'));
+import morgan from "morgan";
+app.use(morgan("combined"));
 
 // here I am importing the database which has been set up in the config folder, in node always put the filename at the end
 
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
 connectDB();
 
 // es6 doesnt wrap the modules in a wrapper so __dirname doesnt exist so i am creating it below with path.resolve
-import path from 'path';
+import path from "path";
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -42,30 +42,28 @@ app.use(express.urlencoded({ extended: false }));
 // below runs the request through each use line of code, adjust as need be
 
 // import ordersRouter from './routes/orders.js';
-import productsRouter from './routes/products.js';
-import usersRouter from './routes/users.js';
-import adminuserRouter from './routes/adminuser.js';
-import ordersRouter from './routes/orders.js';
+import toDosRouter from "./routes/toDosRouter.js";
+import usersRouter from "./routes/users.js";
 
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/adminuser', adminuserRouter);
-app.use('/orders', ordersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/adminuser", adminuserRouter);
+app.use("/orders", ordersRouter);
 
 // Below will serve my images for the products and also uploaded images when new products are
 // created
 
-app.use(express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(505).json('There has been an error');
+  res.status(505).json("There has been an error");
   next();
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('listening on port', port);
+  console.log("listening on port", port);
 });
